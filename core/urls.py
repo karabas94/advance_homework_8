244df08cc525f1b875cedf9a7eae8d4c1de0c775
karabas94 <alkar_94@ukr.net>
2023-04-25 15:14:26 +0300

    -create profile, update profile

--- core/urls.py
+++ core/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from blog.views import RegisterFormView, login_request, logout_request
+from blog.views import RegisterFormView, login_request, logout_request, UpdateProfile, UserProfile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('accounts/register/', RegisterFormView.as_view(), name='register'),
     path('accounts/login/', login_request, name='login'),
+    path('accounts/update_profile/', UpdateProfile.as_view(), name='update_profile'),
+    path('accounts/', UserProfile.as_view(), name='profile'),
     path('logout', logout_request, name="logout"),
 ]
