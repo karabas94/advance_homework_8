3510f8664be975a11bea8af1c58ee46d18172045
karabas94 <alkar_94@ukr.net>
2023-04-26 19:53:14 +0300

    created views, templates for post model

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from .views import PostCreateView, PostUpdateView, PostDeleteView, PostListView, PostDetailView, my_draft, my_post
 
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('post/create/', PostCreateView.as_view(), name='post_form'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('post/', PostListView.as_view(), name='post_list'),
+    path('post_by_<str:user>/', PostListView.as_view(), name='user_post_list'),
+
+    path('my_post', my_post, name='user_my_post_list'),
+    path('my_draft/', my_draft,  name='user_draft_post_list'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,13 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from .forms import RegisterForm
+from .models import Post
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 User = get_user_model()
 
@@ -71,3 +73,70 @@ class UserProfile(LoginRequiredMixin, generic.DetailView):
     def get_object(self, queryset=None):
         user = self.request.user
         return user
+
+
+class PublicProfile(generic.DetailView):
+    model = User
+    template_name = 'registration/public_profile.html'
+
+    def get_object(self, queryset=None):
+        user = User.objects.get(pk=self.kwargs['pk'])
+        return user
+
+
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    fields = ['title', 'short_description', 'text', 'image', 'is_draft', 'is_published']
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Post
+    fields = ['title', 'short_description', 'text', 'image', 'is_draft', 'is_published']
+    success_url = reverse_lazy('blog:index')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author=self.request.user)
+        return queryset
+
+
+class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author=self.request.user)
+        return queryset
+
+
+class PostListView(generic.ListView):
+    model = Post
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        username = self.kwargs.get('user')
+        if username:
+            queryset = queryset.filter(author__username=username)
+        return queryset
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+
+@login_required
+def my_draft(request):
+    drafts = Post.objects.filter(author=request.user, is_draft=True)
+    return render(request, 'blog/user_draft_post_list.html', {'drafts': drafts})
+
+
+@login_required
+def my_post(request):
+    posts = Post.objects.filter(author=request.user, is_published=True)
+    return render(request, 'blog/user_my_post_list.html', {'posts': posts})
