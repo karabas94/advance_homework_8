2c6d02554c4cae92343d2a8675cac7a19e6fe2aa
karabas94 <112809113+karabas94@users.noreply.github.com>
2023-05-04 19:14:53 +0300

    Update apps.py

--- blog/apps.py
+++ blog/apps.py
@@ -1,15 +1,7 @@
 from django.apps import AppConfig
 
-from django.db.models.signals import post_save
 
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
-
-    def ready(self):
-        from .signals import send_notification_comment_mail, send_notification_post_mail, comment_reviewed
-        from .models import Comment, Post
-        post_save.connect(send_notification_comment_mail, sender=Comment)
-        post_save.connect(send_notification_post_mail, sender=Post)
-        post_save.connect(comment_reviewed, sender=Comment)
